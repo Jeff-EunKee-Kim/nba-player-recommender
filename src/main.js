79d0b5f592e90893fd273d1f4493c498c9d840fc
Jeff Kim <eunkeekim@gmail.com>
2019-10-25 16:18:34 -0400

    front end updated

---
+++ src/main.js
@@ -0,0 +1,11 @@
+function find_player() {
+
+    var team = document.getElementById("team").value;
+    var limit = document.getElementById("playerNumber").value;
+    var result = "Players: " + team + "<br>" + "show this many teams: " + limit;
+
+
+
+
+    document.getElementById("players").innerHTML = result;
+}
