c3ae52d0bcf0205cc55a7e8b3b4777245aa6ce1f
Jeff Kim <eunkeekim@gmail.com>
2019-10-30 19:19:04 -0400

    more players

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ function find_player() {
 
     var team = document.getElementById("team").value;
     var limit = document.getElementById("playerNumber").value;
+    
     var result = "Players: " + team + "<br>" + "show this many teams: " + limit;
 
 
