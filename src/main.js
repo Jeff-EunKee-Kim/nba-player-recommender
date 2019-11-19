57a129bff4be7ad629a7a108b9f554f51b0d5932
Daniel Song <songdj98@gmail.com>
2019-11-19 17:00:50 -0500

    new frontend

--- src/main.js
+++ src/main.js
@@ -1,4 +1,53 @@
 
+// finds most similar players to query player
+function find_sim_player() {
+    // get the team, and player query
+    var team = document.getElementById("whichTeam").value;
+    var player = document.getElementById("whichPlayer").value;
+    var limit = document.getElementById("playerNumber").value;
+    
+    console.log(team);
+    console.log(player);
+    console.log(limit)
+
+    testUrl = "https://cors-anywhere.herokuapp.com/https://ipjtqmyurh.execute-api.us-east-1.amazonaws.com/test/ideal/"
+    
+
+    testUrl += "?" + "Team=" + team + "&Limit=" + limit.toString()
+    var url = new URL(testUrl);
+    console.log(testUrl)
+    // var url = new URL("https://17jn058fdh.execute-api.us-east-1.amazonaws.com/test");
+    // var Url = "http://api.ipinfodb.com/v3/ip-city/?key=9d64fcfdfacc213c7ddf4ef911okjjhh97b55e4696be3532bf8302hhhhc09ebad06b&format=json&ip=" + x;
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.send(JSON.stringify({ "Team": team, "Limit": limit }));
+
+    // xhr.send();
+    xhr.onreadystatechange = processRequest;
+    function processRequest(e) {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            // alert(xhr.responseText);
+            response = JSON.parse(xhr.responseText);
+            
+            team = response.Team;
+            players = response.Players;
+            text = "<br>";
+
+            for (var i = 0; i < players.length; i++) {
+                var split = players[i].split(" ");
+                imglink = "https://nba-players.herokuapp.com/players/" + split[1] + "/" + split[0];
+                var xml = new XMLHttpRequest();
+                xml.open('GET', imglink, false);
+                xml.send();
+                var res = xml.responseText;
+            }
+            document.getElementById("players").innerHTML = "Team is: " + team + "<br>" + "Players are: " + text;
+        }
+    }
+}
+
+// finds best matches for query team
 function find_player() {
 
     var team = document.getElementById("team").value;
@@ -65,4 +114,8 @@ function find_player() {
 
 
     // document.getElementById("players").innerHTML = result;
+}
+
+function refresh() {
+    location.reload();
 }
